d3cf81ce21b2ef1ae45d9cb48ad9d3e70ca3c905
Sam <samhum90@gmail.com>
2020-04-28 19:55:44 +0100

    rewrote about List

--- version_2/js/App.js
+++ version_2/js/App.js
@@ -16,8 +16,6 @@ function myFunction() {
   }
 };
 
-
-
 // project Box animations
 document.addEventListener('DOMContentLoaded', () =>{
 
@@ -27,18 +25,24 @@ document.addEventListener('DOMContentLoaded', () =>{
   const projectFour = document.querySelector("#project4");
   const projectFive = document.querySelector("#project5");
   const projectSix = document.querySelector("#project6");
+  const mediumScreen = window.matchMedia("(min-width: 600px) and (max-width: 920px)");
+  const smallScreen = window.matchMedia("(max-width: 600px)");
 
   const petShopGif = document.querySelector("#Pet-shop-gif");
   const petShopDes = document.querySelector("#Pet-shop-des");
+  const techCon = document.querySelectorAll(".tech-container");
   projectOne.addEventListener("mouseover", () => {
-    petShopGif.src = ("images/Pet_shop.gif");
-    petShopDes.classList.add("mouseover-right");
-    // petShopDes.classList.remove("pro-des");
+    if (smallScreen.matches) {
+      petShopDes.classList.add("mouseover-over");
+    }  else {
+      petShopDes.classList.add("mouseover-right");
+    }
+    petShopGif.src = ("Images/Pet_Shop.gif");
     projectOne.style.zIndex = "100";
   });
   projectOne.addEventListener("mouseout", () => {
-    petShopGif.src = ("images/Pet_shop.png");
-    petShopDes.classList.remove("mouseover-right");
+    petShopGif.src = ("Images/Pet_Shop.png");
+    petShopDes.classList.remove("mouseover-left", "mouseover-over", "mouseover-right");
     // petShopDes.classList.add("pro-des")
     projectOne.style.zIndex = "0";
   });
@@ -46,40 +50,55 @@ document.addEventListener('DOMContentLoaded', () =>{
   const cinAppGif = document.querySelector("#cinema-app-pic");
   const cinAppDes = document.querySelector("#cinema-app-des");
   projectTwo.addEventListener("mouseover", () => {
-    cinAppGif.src = ("images/cinema_app.gif");
-    cinAppDes.classList.add("mouseover-left");
+    if (smallScreen.matches) {
+      cinAppDes.classList.add("mouseover-over");
+    }  else {
+      cinAppDes.classList.add("mouseover-left");
+    }
+    cinAppGif.src = ("Images/Cinema_App.gif");
     projectTwo.style.zIndex = "100";
   });
   projectTwo.addEventListener("mouseout", () => {
-    cinAppGif.src = ("images/cinema_app.png");
-    cinAppDes.classList.remove("mouseover-left");
+    cinAppGif.src = ("Images/Cinema_App.png");
+    cinAppDes.classList.remove("mouseover-left", "mouseover-over", "mouseover-right");
     projectTwo.style.zIndex = "0";
   });
 
   const gymAppPic = document.querySelector("#gym-manager-pic");
   const gymAppDes = document.querySelector("#gym-app-des");
-  gymAppPic.addEventListener("mouseover", () => {
-    gymAppPic.src = ("images/gym_app.gif");
-    gymAppDes.classList.add("mouseover-left");
+  projectThree.addEventListener("mouseover", () => {
+    if (mediumScreen.matches) {
+      gymAppDes.classList.add("mouseover-right");
+    } else if (smallScreen.matches) {
+      gymAppDes.classList.add("mouseover-over");
+    }  else {
+      gymAppDes.classList.add("mouseover-left");
+    }
+    gymAppPic.src = ("Images/Gym_App.gif");
     projectThree.style.zIndex = "100";
   });
-  gymAppPic.addEventListener("mouseout", () => {
-    gymAppPic.src = ("images/gym_app.jpg");
-    gymAppDes.classList.remove("mouseover-left");
+  projectThree.addEventListener("mouseout", () => {
+    gymAppPic.src = ("Images/Gym_App.jpg");
+    gymAppDes.classList.remove("mouseover-left", "mouseover-over", "mouseover-right");
     projectThree.style.zIndex = "0";
   });
 
   const ghibliGif = document.querySelector("#ghibli-app-pic");
   const ghibliDes = document.querySelector("#ghibli-app-des");
   projectFour.addEventListener("mouseover", () => {
-    ghibliGif.src = ("images/ghibli_app.gif");
-    ghibliDes.classList.add("mouseover-right");
-    // petShopDes.classList.remove("pro-des");
+    if (mediumScreen.matches) {
+      ghibliDes.classList.add("mouseover-left");
+    } else if (smallScreen.matches) {
+      ghibliDes.classList.add("mouseover-over");
+    } else {
+      ghibliDes.classList.add("mouseover-right");
+    }
+    ghibliGif.src = ("Images/Ghibli_App.gif");
     projectFour.style.zIndex = "100";
   });
   projectFour.addEventListener("mouseout", () => {
-    ghibliGif.src = ("images/ghibli_app.png");
-    ghibliDes.classList.remove("mouseover-right");
+    ghibliGif.src = ("Images/Ghibli_App.png");
+    ghibliDes.classList.remove("mouseover-left", "mouseover-over", "mouseover-right");
     // petShopDes.classList.add("pro-des")
     projectFour.style.zIndex = "0";
   });
@@ -87,13 +106,19 @@ document.addEventListener('DOMContentLoaded', () =>{
   const covidAppGif = document.querySelector("#covid-app-pic");
   const covidAppDes = document.querySelector("#covid-app-des");
   projectFive.addEventListener("mouseover", () => {
-    covidAppGif.src = ("images/COVID_app.gif");
-    covidAppDes.classList.add("mouseover-left");
+    if (mediumScreen.matches) {
+      covidAppDes.classList.add("mouseover-right");
+    } else if (smallScreen.matches) {
+      covidAppDes.classList.add("mouseover-over");
+    } else {
+      covidAppDes.classList.add("mouseover-left");
+    }
+    covidAppGif.src = ("Images/COVID_App.gif");
     projectFive.style.zIndex = "100";
   });
   projectFive.addEventListener("mouseout", () => {
-    covidAppGif.src = ("images/COVID_app.png");
-    covidAppDes.classList.remove("mouseover-left");
+    covidAppGif.src = ("Images/COVID_App.png");
+    covidAppDes.classList.remove("mouseover-left", "mouseover-over", "mouseover-right");
     projectFive.style.zIndex = "0";
   });
   //Responsive navbar
